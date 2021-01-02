c0d5bbf9d48822ffb73ed81204033fdd4c89bf5e
Stefan Lohmaier <stefan@noneus.com>
2021-01-02 20:05:13 +0100

    Initial Commit

---
+++ transbrl_cli/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  transbrl_cli
+//
+//  Created by Stefan Lohmaier on 02.01.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
