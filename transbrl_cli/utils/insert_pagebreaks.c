5503c342b06e49b3aca471f569307e877e32fe97
Stefan Lohmaier <stefan@noneus.com>
2021-01-04 18:52:01 +0100

    insert_pagebreaks compiles

--- transbrl_cli/utils/insert_pagebreaks.c
+++ transbrl_cli/utils/insert_pagebreaks.c
@@ -34,10 +34,10 @@ char *generate_pagenumber(md2brl *data, int num) {
     char *result = (char*)malloc(sizeof(char)*data->width+1);
     int i;
     
-    for (i = 0; i < data->width+1; i++;) result[i] = ' ';
+    for (i = 0; i < data->width+1; i++) result[i] = ' ';
     
     if (size < 0 || result == NULL) return NULL;
-    snprintf(&result[data->width-size], size, "#%d", num)
-    strncat(tesult, "\n", 1);
+    snprintf(&result[data->width-size], size, "#%d", num);
+    strncat(result, "\n", 1);
     return result;
 }
